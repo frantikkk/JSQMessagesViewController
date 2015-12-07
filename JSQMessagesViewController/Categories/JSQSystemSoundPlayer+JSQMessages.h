45898be14198dae089dc3fbd59f212e06876b858
Yury Shalin <Yury_Shalin@epbyminw4627.minsk.epam.com>
2015-12-07 18:22:26 +0300

    Revert import update

--- JSQMessagesViewController/Categories/JSQSystemSoundPlayer+JSQMessages.h
+++ JSQMessagesViewController/Categories/JSQSystemSoundPlayer+JSQMessages.h
@@ -16,7 +16,8 @@
 //  Released under an MIT license: http://opensource.org/licenses/MIT
 //
 
-@import JSQSystemSoundPlayer;
+//@import JSQSystemSoundPlayer;
+#import <JSQSystemSoundPlayer/JSQSystemSoundPlayer.h>
 
 @interface JSQSystemSoundPlayer (JSQMessages)
 
